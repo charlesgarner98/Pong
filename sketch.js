84a4e900ad911854e81e1a6b7ec50adedbb7b4f4
Charles Garner <charles.garner@ada.ac.uk>
2018-10-20 22:02:28 +0100

    Added movement to paddle, score counter and top bounce bar

--- sketch.js
+++ sketch.js
@@ -1,5 +1,9 @@
-var x = 300
-var y = 200
+var score = 0
+var xBall = 300
+var yBall = 200
+var xPaddle = 200
+var paddleSpeed = 20
+var paddleWidth = 200
 
 function setup() {
   createCanvas(600, 400)
@@ -7,7 +11,31 @@ function setup() {
 
 function draw() {
   background(0)
+  fill(255)
 
-  ellipse(x,y,10,10)
+  //Non moving
+  rect (-1,-1,601,7)
+  text('Score: ' + score, 10, 30)
 
+  isPaddleAtEdge()
+
+  ellipse(xBall,yBall,10,10)
+  rect(xPaddle, 385, paddleWidth, 10)
+
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+      xPaddle = xPaddle - paddleSpeed
+  } else if (keyCode === RIGHT_ARROW) {
+      xPaddle = xPaddle + paddleSpeed
+  }
+}
+
+function isPaddleAtEdge() {
+  if(xPaddle > 600 - paddleWidth) {
+    xPaddle = 600 - paddleWidth
+  } else if (xPaddle < 0) {
+    xPaddle = 0
+  }
 }
